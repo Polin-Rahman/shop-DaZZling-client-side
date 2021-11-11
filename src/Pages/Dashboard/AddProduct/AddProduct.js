be7a8c1e38c9c3bca2e7d51166cf4554e6f34b7c
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2021-11-12 04:02:17 +0600

    add product & manage products done

--- src/Pages/Dashboard/AddProduct/AddProduct.js
+++ src/Pages/Dashboard/AddProduct/AddProduct.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import './AddProduct.css';
 
 const AddProduct = () => {
+
+    const { register, handleSubmit, reset } = useForm();
+
+    const onSubmit = data => {
+        fetch('http://localhost:5000/addProduct', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.insertedId) {
+                    alert('New product added Successfully');
+                    reset();
+                }
+            })
+    }
+
     return (
-        <div>
-            <h3>add product</h3>
+        <div className="container addProductContainer">
+            <h3 className="text-center my-5 fst-italic">Add a new product</h3>
+
+            <div className="d-flex justify-content-center">
+                <div>
+                    <form className="order-form" onSubmit={handleSubmit(onSubmit)}>
+                        <p className="mb-1"><small>New product name:</small></p>
+
+                        <input placeholder="Product Name" defaultValue="" {...register("name", { required: true })} />
+
+                        <p className="mb-1"><small>Price:</small></p>
+
+                        <input placeholder="Price" defaultValue="" {...register("price", { required: true })} />
+
+                        <p className="mb-1"><small>Add image url:</small></p>
+
+                        <input placeholder="Image URL" defaultValue="" {...register("img", { required: true })} />
+
+                        <p className="mb-1"><small>Add detail:</small></p>
+
+                        <input placeholder="Detail" defaultValue="" {...register("detail", { required: true })} />
+
+                        <input type="submit" />
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/ManageProducts/ManageProducts.js
+++ src/Pages/Dashboard/ManageProducts/ManageProducts.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ManageProducts = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/products')
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data);
+            })
+    }, [])
+
+    // delete order
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure, you want to Delet?');
+        if (proceed) {
+            const url = `http://localhost:5000/product/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('Deleted successfully');
+                        const remaining = products.filter(user => user._id !== id);
+                        setProducts(remaining);
+                    }
+                });
+        }
+    }
+
     return (
-        <div>
-            <h3>manage products</h3>
+        <div className="container my-5">
+            <h3 className="text-center fst-italic mb-5">Total Products: {products.length}</h3>
+
+            <div className="row">
+
+                {
+                    products.map(product => (
+                        <div className="col-sm-6">
+
+                            <div className="card mb-3" style={{ 'max-width': '540px' }}>
+                                <div className="row g-0">
+                                    <div className="col-md-3">
+                                        <img src={product.img} className="img-fluid rounded-start" alt="..." />
+                                    </div>
+                                    <div className="col-md-9">
+                                        <div className="card-body">
+                                            <h5 className="card-title">{product.name}</h5>
+                                            <p className="card-text">{product.detail}</p>
+                                            <h6>Price: $ {product.price}</h6>
+                                            <button onClick={() => handleDelete(product._id)} className="btn btn-outline-danger btn-sm">Remove this Product</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    ))
+                }
+
+            </div>
         </div>
     );
 };
