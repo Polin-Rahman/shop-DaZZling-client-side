6e2f7f03f881043ffc99b4411905fbb27c8030c8
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2021-11-12 16:19:44 +0600

    showing reviews on home page

---
+++ src/Pages/Home/Review/Review.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Rating from 'react-rating';
+import './Review.css';
+
+const Review = ({ review }) => {
+    const { name, description, rating } = review;
+    return (
+        <div className="col">
+
+            <div className="card text-center border-0">
+
+                <div className="card-body">
+                    <i className="fas fa-user-circle fs-1 text-secondary"></i>
+                    <h6 className="card-title">{name}</h6>
+                    <p className="card-text">{description}</p>
+                    <Rating
+                        initialRating={rating}
+                        emptySymbol="far fa-star icon-color"
+                        fullSymbol="fas fa-star icon-color"
+                        readonly>
+                    </Rating>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Review;
--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Review from '../Review/Review';
 
 const Reviews = () => {
+
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/reviews')
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data);
+            })
+    }, [])
+
     return (
-        <div>
-            <h2>Reviews Section</h2>
+        <div className="container" style={{ marginTop: '100px' }}>
+            <h1 className="text-center fst-italic" >Customer Reviews</h1>
+
+            <div className="row row-cols-2 row-cols-md-5 g-4 mt-5">
+
+                {
+                    reviews.map(review => <Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
+
+            </div>
         </div>
     );
 };
